8d03b3442b8e0ba1b029b710c9ba5c3b0d0955f4
Enni <cardifftardis@gmail.com>
2018-04-30 12:02:39 +0300

    Finalised output to give total number of lines, added type declarations

--- coursework.hs
+++ coursework.hs
@@ -12,10 +12,11 @@ main = do
     let files = findFiles contents
 
     charList <- newEmptyMVar
+    totalLines <- newEmptyMVar
         
-    fileloop files gap pairAmount Map.empty charList []
+    fileloop files 0 gap pairAmount Map.empty charList []
     finalResult <- takeMVar charList
-    let result = prettify finalResult
+    let result = prettify (fst finalResult) (snd finalResult)
     writeFile "output.txt" (unlines result) 
     
     
@@ -26,38 +27,52 @@ findFiles contents
     | otherwise = (last contents):[]
     
 -- Prettifies the output for the file
-prettify [] = []
-prettify pairList =
-    prettifiedPair:prettify (drop 1 pairList)
+prettify :: (Show a, Show a1, Show a2, Show a3) => [((a1, a), a2)] -> a3 -> [String]
+prettify [] x = []
+prettify pairList totalLines =
+    prettifiedPair:prettify (drop 1 pairList) totalLines
     where pair = pairList !! 0
-          prettifiedPair= ((show (fst (fst pair))) ++ " " ++ (show (snd (fst pair))) ++ " " ++ (show (snd pair)))
+          prettifiedPair= ((show (fst (fst pair))) ++ " " ++ (show (snd (fst pair))) ++ " " ++ (show (snd pair)) ++ " " ++ (show totalLines))
 
+fileloop
+    :: [FilePath]
+     -> Int
+     -> Int
+     -> Int
+     -> Map.Map (Char, Char) Int
+     -> MVar ([((Char, Char), Int)], Int)
+     -> [Map.Map (Char, Char) Int]
+     -> IO ThreadId
 
-fileloop files gap pairAmount chars charList maps =
+fileloop files lineAmount gap pairAmount chars charList maps =
     if length files > 0
         then do inFile <- readFile (files !! 0)
                 forkIO $ do
+                    let fileLines = length (lines inFile)
                     let charMap = returnCharPairs inFile gap
-                    fileloop (tail files) gap pairAmount charMap charList (charMap:maps)
+                    fileloop (tail files) (lineAmount + fileLines) gap pairAmount charMap charList (charMap:maps)
                     exitSuccess
         else forkIO $ do
                 let finalMap = mergeMaps maps
-                putMVar charList (take pairAmount (sortMap finalMap))
+                putMVar charList ((take pairAmount (sortMap finalMap)), lineAmount)
                 exitSuccess
                 
 
 -- Merge the maps in a list
+mergeMaps :: (Num a, Ord k) => [Map.Map k a] -> Map.Map k a
 mergeMaps [x] = x
 mergeMaps mapList =
     mergeMaps ((Map.unionWith (+) (mapList !! 0) (mapList !! 1)):(drop 2 mapList))
     
 -- Return the charactermap of a file
+returnCharPairs :: String -> Int -> Map.Map (Char, Char) Int
 returnCharPairs inFile gap =
     addValuesTogether charPairs
     where charPairs = concat (goThroughLines contents gap)
           contents = lines inFile
                   
 -- Return the unique character pairs in a file
+goThroughLines :: [String] -> Int -> [[((Char, Char), Int)]]
 goThroughLines [] gap = []
 goThroughLines contents gap =
     newChars:(goThroughLines (tail contents) gap)
@@ -81,8 +96,8 @@ getPair line gap
 -- Add together values on the same keys
 -- Call this to get the eventual number of occurrences for each character pair
 addValuesTogether :: (Num v, Ord c1, Ord c2) => [((c1,c2),v)] -> Map.Map (c1,c2) v
---(Num a, Num k, Ord k) => [(k,a)] -> Map.Map k a
 addValuesTogether charList = Map.fromListWith (+) charList
 
+sortMap :: Ord a => Map.Map (t, t1) a -> [((t, t1), a)]
 sortMap charMap =
     sortBy (\((k1, k2),v1) ((k3, k4), v2) -> v2 `compare` v1) $ Map.toList charMap
